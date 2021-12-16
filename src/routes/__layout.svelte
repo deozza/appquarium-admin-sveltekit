c7f00c695708e48755e2327a4b0314278cb16498
deozza <deozza@gmail.com>
2021-12-16 22:37:46 +0100

    better layout

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,18 +1,9 @@
-<script context="module" lang="ts">
-    import { initFirebase } from "../app/adapters/firebase/initFirebase";
-
-    export async function load({ page, fetch, session, context }) {
-        await initFirebase();
-        return {
-            props: {},
-        };
-    }
-</script>
-
 <script lang="ts">
 
     import "../app.css";
 
 </script>
 
-<slot></slot>
+<main class="bg-gray-200 min-w-max min-h-screen ">
+    <slot></slot>
+</main>
