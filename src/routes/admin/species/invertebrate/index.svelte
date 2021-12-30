aec72f8340ffbbd8d97bb65fc8f00df797d93f64
deozza <deozza@gmail.com>
2021-12-30 23:05:11 +0100

    switch from loading in module to loading in actual component

--- src/routes/admin/species/invertebrate/index.svelte
+++ src/routes/admin/species/invertebrate/index.svelte
@@ -1,89 +1,84 @@
-<script context="module" lang="ts">
-    import Result from "../../../../app/utils/useCasesResult/Result";
-    import UserUseCase from "../../../../app/user/useCases/UseCase";
-    import InvertebrateUseCase from "../../../../app/species/invertebrate/useCases/UseCase";
-
-    /**
-     * @type {import('@sveltejs/kit').Load}
-     */
-    export async function load() {
-        const userUseCase: UserUseCase = new UserUseCase()
-
-        const jwt: Result = userUseCase.getToken()
-
-        const invertebrateUseCase: InvertebrateUseCase = new InvertebrateUseCase()
-
-        const listOfInvertebrates: Result = await invertebrateUseCase.getListOfInvertebrates(jwt.content)
-
-        if (listOfInvertebrates.isFailed()) {
-            for (const error of listOfInvertebrates.errors) {
-                if (error.code === 401) {
-                    userUseCase.logout()
-                    return {
-                        redirect: '/login',
-                        status: 302
-                    }
-                }
-            }
-
-            return {
-                props: {
-                    listOfInvertebrates: []
-                }
-            }
-        }
-        return {
-            props: {
-                listOfInvertebrates: listOfInvertebrates.content
-            }
-        }
-    }
-</script>
-
 <script lang="ts">
     import {header, addInvertebrateButton} from "../../../../components/pages/admin/invertebrate/index/Modeles";
-
     import BaseHeader from "../../../../components/atoms/typography/header/BaseHeader.svelte";
-    import Species from "../../../../app/species/global/entities/Species";
     import BaseButton from "../../../../components/atoms/button/BaseButton.svelte";
 
+    import Species from "../../../../app/species/global/entities/Species";
+
+    import UserUseCase from '../../../../app/user/useCases/UseCase';
+    import InvertebrateUseCase from '../../../../app/species/invertebrate/useCases/UseCase';
+    import Result from '../../../../app/utils/useCasesResult/Result';
+    import UseCaseError from '../../../../app/utils/useCasesResult/types/UseCaseError';
+
+    import {goto} from '$app/navigation';
+
     export let listOfInvertebrates: Array<Species> = []
+    const userUseCase: UserUseCase = new UserUseCase()
+    const jwt: Result = userUseCase.getToken()
+
+    async function loadInvertebrates(): Promise<Array<Species> |  Array<UseCaseError>>{
+        const invertebrateUseCase: InvertebrateUseCase = new InvertebrateUseCase()
+        const listOfInvertebratesResult: Result = await invertebrateUseCase.getListOfInvertebrates(jwt.content)
+
+        if (listOfInvertebratesResult.isFailed()) {
+            for (const error of listOfInvertebratesResult.errors) {
+                if (error.code === 401) {
+                    userUseCase.logout()
+                    return goto('/admin')
+                }
+            }
+
+            return listOfInvertebratesResult.errors
+        }
+
+        listOfInvertebrates = listOfInvertebratesResult.content
+
+        return listOfInvertebrates
+    }
 
 </script>
 
 
 <div class="flex-c" id="content">
     <BaseHeader baseHeaderModel={header}/>
-
-    <template slot="body">
-        <table class="table-auto">
-            <thead>
-            <tr>
-                <th scope="col">#</th>
-                <th scope="col">Nom scientifique</th>
-                <th scope="col">Etat</th>
-                <th scope="col">Créé le</th>
-                <th scope="col">Modifié le</th>
-            </tr>
-            </thead>
-            <tbody>
-            {#each listOfInvertebrates as invertebrate, i}
+    {#await loadInvertebrates()}
+        <p>chargement</p>
+    {:then listOfInvertebrates}
+        <template slot="body">
+            <table class="table-auto">
+                <thead>
                 <tr>
-                    <td>{i + 1}</td>
-                    <td>
-                        <a class="font-semibold text-blue-500 hover:text-blue-700 transition-colors duration-300"
-                           href={invertebrate.computeLinkToSpecies()}
-                           sveltekit:prefetch>{invertebrate.computeName()}</a>
-                    </td>
-                    <td>{invertebrate.getPublicationStateContent()}</td>
-                    <td>{invertebrate.created_at.getDate() + '/' + invertebrate.created_at.getMonth() + '/' + invertebrate.created_at.getFullYear() + ' ' + invertebrate.created_at.getHours() + ':' + invertebrate.created_at.getMinutes()}</td>
-                    <td>{invertebrate.updated_at.getDate() + '/' + invertebrate.updated_at.getMonth() + '/' + invertebrate.updated_at.getFullYear() + ' ' + invertebrate.updated_at.getHours() + ':' + invertebrate.updated_at.getMinutes()}</td>
+                    <th scope="col">#</th>
+                    <th scope="col">Nom scientifique</th>
+                    <th scope="col">Etat</th>
+                    <th scope="col">Créé le</th>
+                    <th scope="col">Modifié le</th>
                 </tr>
-            {/each}
-            </tbody>
-        </table>
-    </template>
+                </thead>
+                <tbody>
+                {#each listOfInvertebrates as invertebrate, i}
+                    <tr>
+                        <td>{i + 1}</td>
+                        <td>
+                            <a class="font-semibold text-blue-500 hover:text-blue-700 transition-colors duration-300"
+                               href={invertebrate.computeLinkToSpecies()}
+                               sveltekit:prefetch>{invertebrate.computeName()}</a>
+                        </td>
+                        <td>{invertebrate.getPublicationStateContent()}</td>
+                        <td>{invertebrate.created_at.getDate() + '/' + invertebrate.created_at.getMonth() + '/' + invertebrate.created_at.getFullYear() + ' ' + invertebrate.created_at.getHours() + ':' + invertebrate.created_at.getMinutes()}</td>
+                        <td>{invertebrate.updated_at.getDate() + '/' + invertebrate.updated_at.getMonth() + '/' + invertebrate.updated_at.getFullYear() + ' ' + invertebrate.updated_at.getHours() + ':' + invertebrate.updated_at.getMinutes()}</td>
+                    </tr>
+                {/each}
+                </tbody>
+            </table>
+        </template>
 
+
+    {:catch errors}
+        {#each errors as error}
+            <p>{error.type}</p>
+        {/each}
+    {/await}
 
     <a href="/admin/species/invertebrate/add">
         <BaseButton baseButtonModel={addInvertebrateButton}/>
--- src/routes/admin/species/plant/index.svelte
+++ src/routes/admin/species/plant/index.svelte
@@ -1,88 +1,81 @@
-<script context="module" lang="ts">
-    import Result from "../../../../app/utils/useCasesResult/Result";
-    import UserUseCase from "../../../../app/user/useCases/UseCase";
-    import PlantUseCase from "../../../../app/species/plant/useCases/UseCase";
-
-    /**
-     * @type {import('@sveltejs/kit').Load}
-     */
-    export async function load() {
-        const userUseCase: UserUseCase = new UserUseCase()
-
-        const jwt: Result = userUseCase.getToken()
-
-        const plantUseCase: PlantUseCase = new PlantUseCase()
-
-        const listOfPlants: Result = await plantUseCase.getListOfPlants(jwt.content)
-
-        if (listOfPlants.isFailed()) {
-            for (const error of listOfPlants.errors) {
-                if (error.code === 401) {
-                    userUseCase.logout()
-                    return {
-                        redirect: '/login',
-                        status: 302
-                    }
-                }
-            }
-
-            return {
-                props: {
-                    listOfPlants: []
-                }
-            }
-        }
-        return {
-            props: {
-                listOfPlants: listOfPlants.content
-            }
-        }
-    }
-</script>
-
 <script lang="ts">
     import {header, addPlantButton} from "../../../../components/pages/admin/plant/index/Modeles";
-
     import BaseHeader from "../../../../components/atoms/typography/header/BaseHeader.svelte";
-    import Species from "../../../../app/species/global/entities/Species";
     import BaseButton from "../../../../components/atoms/button/BaseButton.svelte";
 
-    export let listOfPlant: Array<Species> = []
+    import Species from "../../../../app/species/global/entities/Species";
+
+    import UserUseCase from '../../../../app/user/useCases/UseCase';
+    import PlantUseCase from '../../../../app/species/plant/useCases/UseCase';
+    import Result from '../../../../app/utils/useCasesResult/Result';
+    import UseCaseError from '../../../../app/utils/useCasesResult/types/UseCaseError';
+
+    import {goto} from '$app/navigation';
+
+    export let listOfPlants: Array<Species> = []
+    const userUseCase: UserUseCase = new UserUseCase()
+    const jwt: Result = userUseCase.getToken()
+
+    async function loadPlants(): Promise<Array<Species> | Array<UseCaseError>> {
+        const plantUseCase: PlantUseCase = new PlantUseCase()
+        const listOfPlantsResult: Result = await plantUseCase.getListOfPlants(jwt.content)
+
+        if (listOfPlantsResult.isFailed()) {
+            for (const error of listOfPlantsResult.errors) {
+                if (error.code === 401) {
+                    userUseCase.logout()
+                    return goto('/login')
+                }
+            }
+
+            return listOfPlantsResult.errors
+        }
+
+        listOfPlants = listOfPlantsResult.content
+        return listOfPlants
+    }
 
 </script>
 
 
 <div class="flex-c" id="content">
     <BaseHeader baseHeaderModel={header}/>
-
-    <template slot="body">
-        <table class="table-auto">
-            <thead>
-            <tr>
-                <th scope="col">#</th>
-                <th scope="col">Nom scientifique</th>
-                <th scope="col">Etat</th>
-                <th scope="col">Créé le</th>
-                <th scope="col">Modifié le</th>
-            </tr>
-            </thead>
-            <tbody>
-            {#each listOfPlant as plant, i}
+    {#await loadPlants()}
+        <p>chargement</p>
+    {:then listOfPlants}
+        <template slot="body">
+            <table class="table-auto">
+                <thead>
                 <tr>
-                    <td>{i + 1}</td>
-                    <td>
-                        <a class="font-semibold text-blue-500 hover:text-blue-700 transition-colors duration-300"
-                           href={plant.computeLinkToSpecies()} sveltekit:prefetch>{plant.computeName()}</a>
-                    </td>
-                    <td>{plant.getPublicationStateContent()}</td>
-                    <td>{plant.created_at.getDate() + '/' + plant.created_at.getMonth() + '/' + plant.created_at.getFullYear() + ' ' + plant.created_at.getHours() + ':' + plant.created_at.getMinutes()}</td>
-                    <td>{plant.updated_at.getDate() + '/' + plant.updated_at.getMonth() + '/' + plant.updated_at.getFullYear() + ' ' + plant.updated_at.getHours() + ':' + plant.updated_at.getMinutes()}</td>
+                    <th scope="col">#</th>
+                    <th scope="col">Nom scientifique</th>
+                    <th scope="col">Etat</th>
+                    <th scope="col">Créé le</th>
+                    <th scope="col">Modifié le</th>
                 </tr>
-            {/each}
-            </tbody>
-        </table>
-    </template>
+                </thead>
+                <tbody>
+                {#each listOfPlants as plant, i}
+                    <tr>
+                        <td>{i + 1}</td>
+                        <td>
+                            <a class="font-semibold text-blue-500 hover:text-blue-700 transition-colors duration-300"
+                               href={plant.computeLinkToSpecies()} sveltekit:prefetch>{plant.computeName()}</a>
+                        </td>
+                        <td>{plant.getPublicationStateContent()}</td>
+                        <td>{plant.created_at.getDate() + '/' + plant.created_at.getMonth() + '/' + plant.created_at.getFullYear() + ' ' + plant.created_at.getHours() + ':' + plant.created_at.getMinutes()}</td>
+                        <td>{plant.updated_at.getDate() + '/' + plant.updated_at.getMonth() + '/' + plant.updated_at.getFullYear() + ' ' + plant.updated_at.getHours() + ':' + plant.updated_at.getMinutes()}</td>
+                    </tr>
+                {/each}
+                </tbody>
+            </table>
+        </template>
 
+    {:catch errors}
+        {#each errors as error}
+            <p>{error.type}</p>
+        {/each}
+    {/await}
 
     <a href="/admin/species/plant/add">
         <BaseButton baseButtonModel={addPlantButton}/>
