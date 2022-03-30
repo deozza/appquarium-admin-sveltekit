5ee999a9f1efa6e61ab7b440c512eac4bcfbe0d1
deozzam <deozza@gmail.com>
2022-03-31 01:20:20 +0200

    added I18n translation

---
+++ src/app/utils/i18n/Translation.ts
@@ -0,0 +1,19 @@
+import i18n from "./i18n.json"
+export default class Translation {
+
+    public static translate(keys: Array<string>): string {
+        let translated: object | string = i18n
+        const local: string = navigator.language
+
+
+        for(const key of keys){
+            if(!translated.hasOwnProperty(key)){
+                return keys[keys.length - 1]
+            } 
+
+            translated = translated[key]
+        }
+
+        return translated[local]
+    }
+}
